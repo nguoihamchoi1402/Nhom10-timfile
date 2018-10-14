35227db9eab68028b99881a4a7b988620b23cf99
nguoihamchoi1402 <44137064+nguoihamchoi1402@users.noreply.github.com>
2018-10-14 20:31:55 +0700

    Update CheckFile.java

--- src/checkfile/CheckFile.java
+++ src/checkfile/CheckFile.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package checkfile;
 
 import java.io.BufferedReader;
@@ -15,11 +11,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-
-/**
- *
- * @author Admin
- */
 public class CheckFile {
 
     /**
